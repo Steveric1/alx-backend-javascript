9e391b30849d2252b32a347b7990808881df11d2
Eric <stephen.erich243@gmail.com>
2024-04-23 14:20:06 +0100

    task 2

---
+++ 0x00-ES6_basic/2-arrow.js
@@ -0,0 +1,9 @@
+export default function getNeighborhoodsList () {
+  this.sanFransciscoNeighborhoods = ['SOMA', 'Union Square'];
+
+  const self = this;
+  this.addNeighborhood = (newnnNeighborhood) => {
+    self.sanFransciscoNeighborhoods.push(newnnNeighborhood);
+    return self.sanFransciscoNeighborhoods;
+  };
+}
