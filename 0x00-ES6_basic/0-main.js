d9d16f30e33136f976ee9685529f5599de6bf397
Eric <stephen.erich243@gmail.com>
2024-04-24 06:34:34 +0100

    task 12

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,19 @@
 import createEmployeesObject from './11-createEmployeesObject.js';
+import createReportObject from './12-createReportObject.js';
 
-console.log(createEmployeesObject("Software", [ "Bob", "Sylvie" ]));
+const employees = {
+    ...createEmployeesObject('engineering', ['Bob', 'Jane']),
+    ...createEmployeesObject('marketing', ['Sylvie'])
+};
+
+const report = createReportObject(employees);
+console.log(report.allEmployees);
+console.log(report.getNumberOfDepartments(report.allEmployees));
+
+const Person = {
+    name: 'John',
+    age: 30,
+    country: 'USA',
+};
+const entry = Object.entries(Person);
+console.log(entry);
