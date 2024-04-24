6cf29b6827308349a0b677e440003f63cfd5f009
Eric <stephen.erich243@gmail.com>
2024-04-24 09:02:29 +0100

    task 13

--- 0x00-ES6_basic/0-main.js
+++ 0x00-ES6_basic/0-main.js
@@ -1,3 +1,5 @@
+import createIteratorObject from "./100-createIteratorObject.js";
+
 import createEmployeesObject from './11-createEmployeesObject.js';
 import createReportObject from './12-createReportObject.js';
 
@@ -7,13 +9,9 @@ const employees = {
 };
 
 const report = createReportObject(employees);
-console.log(report.allEmployees);
-console.log(report.getNumberOfDepartments(report.allEmployees));
 
-const Person = {
-    name: 'John',
-    age: 30,
-    country: 'USA',
-};
-const entry = Object.entries(Person);
-console.log(entry);
+const reportWithIterator = createIteratorObject(report);
+
+for (const item of reportWithIterator) {
+    console.log(item);
+}
