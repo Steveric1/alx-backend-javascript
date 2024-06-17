208710d4e224506ab1d96c3a23f9fa71946c48b4
Eric <stephen.erich243@gmail.com>
2024-06-17 15:07:35 +0000

    data manipulation

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,8 +1,6 @@
 export default function getListStudentIds(array) {
-    if (!Array.isArray(array)) {
-      return [];
-    }
-    return array.map((item) => item.id);
+  if (!Array.isArray(array)) {
+    return [];
+  }
+  return array.map((item) => item.id);
 }
-
-  
