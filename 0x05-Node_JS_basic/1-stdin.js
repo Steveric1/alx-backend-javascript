dda695c0bedb9a5bf1b655326bc820f6b3b1aa6f
Eric <stephen.erich243@gmail.com>
2024-06-18 23:41:12 +0000

    node basics

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,15 +2,13 @@ process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-// read data
 process.stdin.on('readable', () => {
   const chunk = process.stdin.read();
   if (chunk !== null) {
-    process.stdout.write('Your name is: ' + chunk);
+    process.stdout.write(`Your name is: ${chunk}`);
   }
 });
 
-// close
 process.stdin.on('end', () => {
-  process.stdout.write('his important software is now closing\n');
+  process.stdout.write('This important software is now closing\n');
 });
